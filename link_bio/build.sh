132605f76f3e38b8c13311de1953c4a92c5288b8
AndresColoniaMir <andres.colonia01@usc.edu.co>
2024-02-27 09:41:41 -0500

    State-URL

--- link_bio/build.sh
+++ link_bio/build.sh
@@ -3,7 +3,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-API_URL=https://andrescolonia-web.up.railway.app/ reflex export --frontend-only
+API_URL=https://andrescolonia-web.up.railway.app reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
